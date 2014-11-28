85557f4366dc8f24d69c092ff06645d4030f79e9
Mingyu Cui <malvin.cui@gmail.com>
2014-11-28 17:14:03 +0800

    Initial Commit

---
+++ FakeZhihuDaily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FakeZhihuDaily
+//
+//  Created by CuiMingyu on 14/11/28.
+//  Copyright (c) 2014年 gnou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FakeZhihuDaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FakeZhihuDaily
+//
+//  Created by CuiMingyu on 14/11/28.
+//  Copyright (c) 2014年 gnou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
