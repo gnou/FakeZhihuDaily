5e2e8ba7c9fcb8df31c8389838ab5392b04ada17
Mingyu Cui <malvin.cui@gmail.com>
2015-01-22 09:14:38 +0800

    Prepare for Swift

---
+++ FakeZhihuDaily/StoriesList.swift
@@ -0,0 +1,9 @@
+//
+//  StoriesList.swift
+//  FakeZhihuDaily
+//
+//  Created by CuiMingyu on 15/1/9.
+//  Copyright (c) 2015年 gnou. All rights reserved.
+//
+
+import Foundation
--- Podfile
+++ Podfile
@@ -1,6 +1,8 @@
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.1'
 
+pod 'AFNetworking'
+pod 'Mantle'
 pod 'TSMessages'
 pod 'ReactiveCocoa'
 pod 'SDWebImage'
