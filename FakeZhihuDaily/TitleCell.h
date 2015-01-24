e1eda048735cf706a1cbe284ae3eaa9ee40552de
Mingyu Cui <malvin.cui@gmail.com>
2015-01-24 16:23:02 +0800

    Copy TitleCell.h/m into project directory

---
+++ FakeZhihuDaily/TitleCell.h
@@ -0,0 +1,15 @@
+//
+//  TitleTableViewCell.h
+//  Fake Zhihu Daily
+//
+//  Created by CuiMingyu on 14/11/28.
+//  Copyright (c) 2014年 gnou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TitleCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *titleImageView;
+
+@end
---
+++ FakeZhihuDaily/TitleCell.m
@@ -0,0 +1,23 @@
+//
+//  TitleTableViewCell.m
+//  Fake Zhihu Daily
+//
+//  Created by CuiMingyu on 14/11/28.
+//  Copyright (c) 2014年 gnou. All rights reserved.
+//
+
+#import "TitleCell.h"
+
+@implementation TitleCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
